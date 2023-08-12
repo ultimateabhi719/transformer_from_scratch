6adee58ae28dc712f5ad6cf132bcf871c624ad84
Abhishek Agarwal <>
2023-08-12 12:01:22 +0000

    save token_size.csv in hash directory

--- src/pytorch_transformer/dataset.py
+++ src/pytorch_transformer/dataset.py
@@ -1,6 +1,10 @@
 
 import os
 import sys
+from typing import Dict, Any
+import hashlib
+
+import json
 import pandas as pd
 
 from tqdm.auto import tqdm
@@ -8,13 +12,12 @@ tqdm.pandas()
 
 import torch
 from torch.utils.data import Dataset
-from datasets import load_dataset
+from datasets import load_dataset, load_from_disk
 
 def _explore_dataset(dataset, hi_tokenizer, en_tokenizer, csv_len = None):
     if csv_len is not None and os.path.exists(csv_len):
         return pd.read_csv(csv_len)
 
-    # dataset = load_dataset(dataset_path)['train']
     hi_tokenizer, lang_from = hi_tokenizer
     en_tokenizer, lang_to = en_tokenizer
 
@@ -33,21 +36,43 @@ def _explore_dataset(dataset, hi_tokenizer, en_tokenizer, csv_len = None):
         df.to_csv(csv_len, index=False)
     return df
 
+
+def dict_hash(dictionary: Dict[str, Any]) -> str:
+    """MD5 hash of a dictionary."""
+    dhash = hashlib.md5()
+    # We need to sort arguments so {'a': 1, 'b': 2} is
+    # the same as {'b': 2, 'a': 1}
+    encoded = json.dumps(dictionary, sort_keys=True).encode()
+    dhash.update(encoded)
+    return dhash.hexdigest()
+
+
+
 class TransformerDataset(Dataset):
     """
         split : 'train', 'validation', 'test'
     """
-    def __init__(self, dataset_path, split, hi_tokenizer, en_tokenizer, max_len = None, seqlen_csv = None, subset = None):
-        self.dataset = load_dataset(*dataset_path)[split]
-        self.hi_tokenizer, self.lang_from = hi_tokenizer
-        self.en_tokenizer, self.lang_to = en_tokenizer
+    def __init__(self, data_params, split, hi_tokenizer, en_tokenizer, max_len = None, subset = None):
+        if data_params['dataset_path'][0] == "local":
+            self.dataset = load_from_disk(*data_params['dataset_path'][1])[split]
+        else:
+            self.dataset = load_dataset(*data_params['dataset_path'][1])[split]
+        # self.dataset = load_dataset(data_params['dataset_path'])[split]
+        self.hi_tokenizer = hi_tokenizer
+        self.en_tokenizer = en_tokenizer
+
+        self.lang_from, self.lang_to = data_params.pop('lang', None)
+
+        eda_path = f'eda/{dict_hash(data_params)}'
+        seqlen_csv = f"{eda_path}/{split}.csv"
+
+        if not os.path.exists(eda_path):
+            os.makedirs(eda_path)
+            json.dump(data_params, open(f"{eda_path}/data_params.json", "w"))
+        if not os.path.exists(seqlen_csv):
+            df = _explore_dataset(self.dataset, (self.hi_tokenizer, self.lang_from), (self.en_tokenizer, self.lang_to), csv_len = seqlen_csv)
 
         if max_len:
-            assert seqlen_csv is not None
-
-            if not os.path.exists(seqlen_csv):
-                df = _explore_dataset(self.dataset, (self.hi_tokenizer, self.lang_from), (self.en_tokenizer, self.lang_to), csv_len = seqlen_csv)
-
             df = pd.read_csv(seqlen_csv)
             self.dataset = torch.utils.data.Subset(self.dataset, indices = list(df['index'][(df.hi<=max_len//2) & (df.en<=max_len//2)]))
 
